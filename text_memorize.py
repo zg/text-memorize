a27c3688616cbf4483bea6459b8bfa5de3416e91
Zack Zatkin-Gold <zzatkin@gmail.com>
2015-09-01 23:42:41 -0400

    adds some empty lines for readability

--- text_memorize.py
+++ text_memorize.py
@@ -9,11 +9,14 @@ import sys
 parser = argparse.ArgumentParser(description="""A tool to help memorize some
 text! When provided with a file, this program will remove random words from
 each line of text and ask you to provide the words that were removed.""")
+
 parser.add_argument('--no-color', action='store_true',
                     help='hide colorful underlining')
+
 parser.add_argument('--a', dest='tries', default=3,
                     help=('number of tries to allow per word '
                          '(0 for unlimited tries, default: 3)'))
+
 parser.add_argument('filename', metavar='filename', type=str,
                     help='the text file')
 
@@ -22,27 +25,35 @@ args = parser.parse_args()
 try:
     with codecs.open(args.filename, 'r', 'utf-8') as f:
         current_line = ''
+
         for line in f:
             missing_words = []
             new_word = u''
+
             for word in line.split(' '):
                 show_word = random.randrange(10) > 4
+
                 for char in word:
                     if (char.isalpha() and show_word) or not char.isalpha():
                         current_line += char
                     else:
                         new_word += char
+
                         if not args.no_color:
                             current_line += '\033[91m'
+
                         current_line += '_'
+
                         if not args.no_color:
                             current_line += '\033[0m'
                 if word[-1] != '\n':
                     current_line += ' '
+
                 if len(new_word):
                     missing_words.append(new_word)
                     new_word = u''
             tries = 0
+
             while len(missing_words):
                 
                 next_word = missing_words[0]
